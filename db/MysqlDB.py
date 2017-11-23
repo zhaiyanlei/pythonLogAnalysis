3bbda96d1232f94f03663e9350df87eda450d300
zhaiyanlei <724306862@qq.com>
2017-11-23 18:14:01 +0800

    日志统计完成

--- db/MysqlDB.py
+++ db/MysqlDB.py
@@ -8,8 +8,9 @@ import MySQLdb
 import conf.conf as conf
 import ConfigParser
 
-#获取最后统计的时间
-def getLastTime():
+
+# 获取最后统计的时间
+def getStartTime():
     cf = get_cf()
     conn = getConn(cf)
     cur = conn.cursor();
@@ -20,7 +21,8 @@ def getLastTime():
     conn.close()
     return data[2]
 
-#修改最后统计时间
+
+# 修改最后统计时间
 def updateLastTime(lasttime):
     cf = get_cf()
     conn = getConn(cf)
@@ -30,38 +32,59 @@ def updateLastTime(lasttime):
     cur.close()
     conn.commit()
     conn.close()
-    
+
+
+def saveStastics(ads):
+    sql = "insert into ad_stastics(apk_channel_id, game_id, type, source, subsource,\
+     visit, finish, click, ipload, ipfinish, ipclick, collect_datetime, load_time) \
+     values('%s','%s','%s', '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
+    cf = get_cf()
+    conn = getConn(cf)
+    cur = conn.cursor();
+    cur.execute(sql % (ads.id, ads.gameId, ads.type, ads.source, ads.subsource, ads.load, \
+                ads.finish, ads.click, len(ads.ipLoad), len(ads.ipfinish), len(ads.ipclick), ads.collect_datetime, ads.load_time))
+    cur.close()
+    conn.commit()
+    conn.close()
+
+
 #######数据库配置#######
 def get_cf():
         cf = ConfigParser.ConfigParser()
         cf.read(conf.db_conf_path)
         return cf
 
-#获取数据库主机名
+
+# 获取数据库主机名
 def getHost(cf):
-    return cf.get('db','host')
+    return cf.get('db', 'host')
 
-#获取数据库端口号
+
+# 获取数据库端口号
 def getPort(cf):
-    return cf.get('db','port')
+    return cf.get('db', 'port')
 
-#获取数据库用户名
+
+# 获取数据库用户名
 def getUser(cf):
-    return cf.get('db','user')
+    return cf.get('db', 'user')
 
-#获取数据库密码
+
+# 获取数据库密码
 def getPasswd(cf):
-    return cf.get('db','passwd')
+    return cf.get('db', 'passwd')
 
-#获取数据库名称
+
+# 获取数据库名称
 def getDbName(cf):
-    return cf.get('db','dbName')
+    return cf.get('db', 'dbName')
 
-#获取数据库连接
+
+# 获取数据库连接
 def getConn(cf):
-    conn =  MySQLdb.connect(host = getHost(cf),
-                           port = int(getPort(cf)),
-                           user = getUser(cf),
-                           passwd = getPasswd(cf),
-                           db = getDbName(cf));
+    conn = MySQLdb.connect(host=getHost(cf),
+                           port=int(getPort(cf)),
+                           user=getUser(cf),
+                           passwd=getPasswd(cf),
+                           db=getDbName(cf));
     return conn
--- main/stastics.py
+++ main/stastics.py
@@ -10,79 +10,175 @@ import db.MysqlDB as db
 from file.getFile import getFilename
 import datetime
 import os
+from db.adStastics import ADStastics
 
-#加载日志配置
+# 加载日志配置
 logging.config.fileConfig("conf/log.conf")
 logger = logging.getLogger("stastics")
 
-#统计开始
+load_dictionary = {}
+finish_dictionary = {}
+click_dictionary = {}
+
+# 统计开始
 def stastics():
+    load_dictionary.clear()
+    finish_dictionary.clear()
+    click_dictionary.clear()
     now = datetime.datetime.now()
-    maxTime = timeformat(now)
-    #获取当前需要统计的日志文件
-    #上一次统计的时间
-    lasttime = db.getLastTime();
-    logger.info(lasttime)
-    filename = getFilename(lasttime)
-    if not os.path.exists(filename):
-        print lasttime,maxTime
-        #文件是当天文件，时间不加一天
-        if cmp(lasttime.strftime('%Y-%m-%d'), maxTime.strftime('%Y-%m-%d')) == 0:
+    nextStartTime = timeQuarterformat(now)  # 获取当前时间，根据当前时间确定下次开始时间
+    starttime = db.getStartTime()  # 本次统计开始的时间
+    logger.info('本次开始统计时间:%s', starttime)
+    filename = getFilename(starttime)  # 获取当前需要统计的日志文件
+    if not os.path.exists(filename):  # 文件不存在
+        logger.info('文件不存在:%s,统计的开始时间为%s', filename, starttime)
+        if cmp(starttime.strftime('%Y-%m-%d'), nextStartTime.strftime('%Y-%m-%d')) >= 0:
+            # 本次开始时间与下次开始时间相同，认为是当天文件不存在，返回
             return
-        lasttime = lasttime + datetime.timedelta(days=1)
-        db.updateLastTime(lasttime.strftime('%Y-%m-%d'))
-        stastics()
-        return;
-    endtime = timeformat(lasttime)
-    logger.info("开始解析配置文件：%s", filename)
+        else:
+            # 本次开始统计时间不是当天，文件不存在即跳过该天的日志，开始日期+1的日志
+            starttime = starttime + datetime.timedelta(days=1)
+            db.updateLastTime(starttime.strftime('%Y-%m-%d'))
+            stastics()
+            return;
+    endtime = timeQuarterformat(nextStartTime)  # 本次统计结束的时间
+    logger.info("开始解析配置文件：%s,统计的开始时间为%s", filename, starttime)
     try:
         fo = open(filename)
-        #逐行读取日志,判断时间是否满足
+        # 逐行读取日志,判断时间是否满足
         for line in fo.readlines():
             param = line.split("#")
-            time = datetime.datetime.strptime(param[0],'%Y-%m-%d %H:%M:%S,%f');
-            if cmp(time, maxTime) <= 0:
-                handleLine(param)
-                endtime = timeformat(time)
+            time = datetime.datetime.strptime(param[0], '%Y-%m-%d %H:%M:%S,%f')  # 解析日志中的时间
+            if time < starttime:
+                continue
+            if time < nextStartTime:
+                handleLine(param, time)
+                endtime = timeQuarterformat(time)
     except IOError :
         logger.info('file not found:%s', filename)
         db.updateLastTime(str(endtime))
     finally:
-        #保存最后统计时间，
-        if cmp(lasttime.strftime('%Y-%m-%d'), maxTime.strftime('%Y-%m-%d')) < 0:
-            db.updateLastTime(str(endtime.strftime('%Y-%m-%d') + datetime.timedelta(days=1)))
-            #读取下一天的日志
-            stastics()
+        savedicts()
+        # 保存最后统计时间，
+        if cmp(starttime.strftime('%Y-%m-%d'), nextStartTime.strftime('%Y-%m-%d')) < 0:
+            nexttime = endtime + datetime.timedelta(days=1)
+            nexttime = datetime.datetime(nexttime.year, nexttime.month, nexttime.day, 0, 0, 0)
+            db.updateLastTime(str(nexttime))
+            stastics()# 读取下一天的日志
         else:
-            db.updateLastTime(str(endtime))
-        
-#解析每一行日志
-def handleLine(param):
+            nexttime = timeQuarterformat(nextStartTime)
+            db.updateLastTime(str(nexttime))
+
+
+endQuarter = None
+
+# 解析每一行日志
+def handleLine(param, time):
     if param == None:
         return
+    global endQuarter
+    if endQuarter == None:
+        startQuarter = timeQuarterformat(time)
+        endQuarter = startQuarter + datetime.timedelta(minutes=15)
+    if time >= endQuarter:
+        savedicts()
+        db.updateLastTime(str(endQuarter))
+        startQuarter = timeQuarterformat(time)
+        endQuarter = startQuarter + datetime.timedelta(minutes=15)
+    
     if param[3] == 'load':
-        stasticsLoad(param)
+        stasticsLoad(param, time)
     elif param[3] == 'finish':
-        stasticsFinish(param)
+        stasticsFinish(param, time)
     elif param[3] == 'click':
-        stasticsClick(param)
+        stasticsClick(param, time)
     else:
         return
 
-#统计加载
-def stasticsLoad(param):
-    return
 
-#统计加载完成
-def stasticsFinish(param):
-    return
+# 统计加载
+def stasticsLoad(param, time):
+    #2017-11-22 11:19:25,874#INFO#http-8019-exec-5#load#129#143#201#jrtt##101.231.117.234#
+    collect_time = timeQuarterformat(time)
+    key = param[4] + param[5] + param[6] + collect_time.strftime('%Y-%m-%d %H:%M:%S') # key格式：gameid + id + type +统计时间段的开始时间
+    ads = None
+    if load_dictionary.has_key(key):
+        ads = load_dictionary.get(key)
+    else:
+        ads = ADStastics()
+        ads.id = param[5]
+        ads.gameId = param[4]
+        ads.type = param[6]
+        ads.collect_datetime = collect_time
+        ads.source = param[7]
+        ads.subsource = param[8]
+    ads.load += 1
+    if ads.ipLoad.count(param[9]) == 0:
+        ads.ipLoad.append(param[9])
+    load_dictionary[key] = ads # 新增或更新dict
 
-#统计点击
-def stasticsClick(param):
-    return
 
-#获取所给时间前一刻钟的时间点
-def timeformat(time):
+# 统计加载完成
+def stasticsFinish(param, time):
+    #2017-11-22 11:19:25,874#INFO#http-8019-exec-5#load#129#143#201#jrtt##101.231.117.234#
+    collect_time = timeQuarterformat(time)
+    key = param[4] + param[5] + param[6] + collect_time.strftime('%Y-%m-%d %H:%M:%S') # key格式：gameid + id + type +统计时间段的开始时间
+    ads = None
+    if finish_dictionary.has_key(key):
+        ads = finish_dictionary.get(key)
+    else:
+        ads = ADStastics()
+        ads.id = param[5]
+        ads.gameId = param[4]
+        ads.type = param[6]
+        ads.collect_datetime = collect_time
+        ads.source = param[7]
+        ads.subsource = param[8]
+    ads.finish += 1
+    ads.load_time += int(param[10])
+    if ads.ipfinish.count(param[9]) == 0:
+        ads.ipfinish.append(param[9])
+    finish_dictionary[key] = ads # 新增或更新dict
+
+
+# 统计点击
+def stasticsClick(param, time):
+    #2017-11-22 11:19:25,874#INFO#http-8019-exec-5#load#129#143#201#jrtt##101.231.117.234#
+    collect_time = timeQuarterformat(time)
+    key = param[4] + param[5] + param[6] + collect_time.strftime('%Y-%m-%d %H:%M:%S') # key格式：gameid + id + type +统计时间段的开始时间
+    ads = None
+    if click_dictionary.has_key(key):
+        ads = click_dictionary.get(key)
+    else:
+        ads = ADStastics()
+        ads.id = param[5]
+        ads.gameId = param[4]
+        ads.type = param[6]
+        ads.collect_datetime = collect_time
+        ads.source = param[7]
+        ads.subsource = param[8]
+    ads.click += 1
+    if ads.ipclick.count(param[9]) == 0:
+        ads.ipclick.append(param[9])
+    click_dictionary[key] = ads # 新增或更新dict
+
+# 保存统计结果
+def savedicts():
+    global endQuarter
+    logger.info("当前统计时间段为：%s" % endQuarter)
+    for key in load_dictionary:
+        db.saveStastics(load_dictionary[key])
+    for key in finish_dictionary:
+        db.saveStastics(finish_dictionary[key])
+    for key in click_dictionary:
+        db.saveStastics(click_dictionary[key])
+    load_dictionary.clear()
+    finish_dictionary.clear()
+    click_dictionary.clear()
+
+
+# 获取所给时间前一刻钟的时间点
+def timeQuarterformat(time):
     minu = 0
     minute = time.minute
     if minute < 15:
@@ -94,4 +190,4 @@ def timeformat(time):
     else:
         minu = 45
     date = time.strftime('%Y-%m-%d %H:')
-    return datetime.datetime.strptime(date + str(minu) + ":00",'%Y-%m-%d %H:%M:%S')
+    return datetime.datetime.strptime(date + str(minu) + ":00", '%Y-%m-%d %H:%M:%S')
